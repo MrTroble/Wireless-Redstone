272cd18dff9c544520230ad9c2923b5e64b3dd1b
MrTroble <nifroh56@gmail.com>
2021-07-12 16:10:34 +0200

    Finished chunk loading system

--- src/main/java/eu/gir/gircredstone/tile/TileRedstoneEmitter.java
+++ src/main/java/eu/gir/gircredstone/tile/TileRedstoneEmitter.java
@@ -6,6 +6,8 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.ChunkProviderServer;
 
 public class TileRedstoneEmitter extends TileEntity {
 
@@ -30,9 +32,22 @@ public class TileRedstoneEmitter extends TileEntity {
 	public void redstoneUpdate(final boolean enabled) {
 		if (linkedpos != null) {
 			System.out.println("RSUP: " + enabled);
+			final boolean flag = !world.isBlockLoaded(linkedpos);
+			Chunk lchunk = null;
+			if(flag) {
+				final Chunk chunk = world.getChunkFromBlockCoords(linkedpos);
+				final ChunkProviderServer provider = (ChunkProviderServer) world.getChunkProvider();
+				lchunk = provider.loadChunk(chunk.x, chunk.z);
+				if(lchunk == null)
+					return;
+			}
 			final IBlockState state = world.getBlockState(linkedpos);
 			if (state.getBlock() instanceof BlockRedstoneAcceptor) {
 				world.setBlockState(linkedpos, state.withProperty(BlockRedstoneAcceptor.POWER, enabled));
+			}
+			if(lchunk != null) {
+				final ChunkProviderServer provider = (ChunkProviderServer) world.getChunkProvider();
+				provider.queueUnload(lchunk);
 			}
 		}
 	}
