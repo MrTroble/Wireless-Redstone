bee28f7cff13c183949c6e5b722c27ecafbf9bb9
MrTroble <nifroh56@gmail.com>
2021-08-28 12:16:37 +0200

    optimized modles

--- src/main/resources/assets/gircredstone/optimize.py
+++ src/main/resources/assets/gircredstone/optimize.py
@@ -5,6 +5,7 @@ for root, dirs, files in os.walk("."):
     for file in files:
         if not file.endswith(".json"): continue
         fullPath = os.path.join(root, file)
-        with open(fullPath, "w+") as fp:
+        with open(fullPath, "r") as fp:
             jobj = json.load(fp)
+        with open(fullPath, "w") as fp:
             json.dump(jobj, fp)
