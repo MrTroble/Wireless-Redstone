a8f0af7974639f308fb8bf2527e7583e14ebbb41
Nico Froehlich <nicofroehlich@icloud.com>
2022-05-29 20:56:23 +0200

    feat: Blocks are now breakable with an iron pickaxe

--- src/main/java/eu/gir/gircredstone/block/BlockBasic.java
+++ src/main/java/eu/gir/gircredstone/block/BlockBasic.java
@@ -5,10 +5,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Material;
 
 public class BlockBasic extends Block {
-
+	
 	public BlockBasic(final String name) {
-		super(Properties.of(Material.METAL));
+		super(Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(1.5F, 6.0F));
 		this.setRegistryName(GIRCRedstoneMain.MODID, name);
 	}
-
+	
 }
