41f810fab6a4fbf623a8b836a3fe817933e74566
Nico Froehlich <nicofroehlich@icloud.com>
2022-06-11 11:44:11 +0200

    fix: compile errors

--- src/main/java/eu/gir/gircredstone/tile/TileRedstoneEmitter.java
+++ src/main/java/eu/gir/gircredstone/tile/TileRedstoneEmitter.java
@@ -9,24 +9,23 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 
 public class TileRedstoneEmitter extends TileEntity {
-
+	
 	public TileRedstoneEmitter() {
 		super(GIRCInit.EMITER_TILE);
 	}
-
+	
 	private BlockPos linkedpos = null;
-
+	
 	@Override
-	public void load(final CompoundTag nbt) {
-		super.load(nbt);
-		this.linkedpos = Linkingtool.readBlockPosFromNBT(nbt);
+	public void load(final BlockState p_230337_1_, final CompoundNBT p_230337_2_) {
+		super.load(p_230337_1_, p_230337_2_);
+		this.linkedpos = Linkingtool.readBlockPosFromNBT(p_230337_2_);
 	}
 	
 	@Override
-	public CompoundTag save(final CompoundTag nbt) {
-		super.save(nbt);
-		Linkingtool.writeBlockPosToNBT(linkedpos, nbt);
-		return nbt;
+	public CompoundNBT save(final CompoundNBT p_189515_1_) {
+		Linkingtool.writeBlockPosToNBT(linkedpos, p_189515_1_);
+		return super.save(p_189515_1_);
 	}
 	
 	public boolean link(final BlockPos pos) {
@@ -35,18 +34,18 @@ public class TileRedstoneEmitter extends TileEntity {
 		this.linkedpos = pos;
 		return true;
 	}
-
+	
 	public boolean unlink() {
 		if (this.linkedpos == null)
 			return false;
 		this.linkedpos = null;
 		return true;
 	}
-
+	
 	public BlockPos getLinkedPos() {
 		return this.linkedpos;
 	}
-
+	
 	public void redstoneUpdate(final boolean enabled) {
 		if (linkedpos != null) {
 			final BlockState state = level.getBlockState(linkedpos);
@@ -55,5 +54,5 @@ public class TileRedstoneEmitter extends TileEntity {
 			}
 		}
 	}
-
+	
 }
