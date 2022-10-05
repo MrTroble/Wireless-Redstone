738b7bcdad2d39f148dfe995dbdf32ff3bd86c1e
Nico Froehlich <nicofroehlich@icloud.com>
2022-10-05 23:17:44 +0200

    feat: added remote activator

--- src/main/java/eu/gir/gircredstone/init/GIRCInit.java
+++ src/main/java/eu/gir/gircredstone/init/GIRCInit.java
@@ -6,6 +6,7 @@ import eu.gir.gircredstone.GIRCRedstoneMain;
 import eu.gir.gircredstone.block.BlockRedstoneAcceptor;
 import eu.gir.gircredstone.block.BlockRedstoneEmitter;
 import eu.gir.gircredstone.item.Linkingtool;
+import eu.gir.gircredstone.item.RemoteActivator;
 import eu.gir.gircredstone.tile.TileRedstoneEmitter;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -22,29 +23,43 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class GIRCInit {
-	
-	public static final DeferredRegister<Item> ITEM_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, GIRCRedstoneMain.MODID);
-	public static final DeferredRegister<Block> BLOCK_REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, GIRCRedstoneMain.MODID);
-	public static final DeferredRegister<BlockEntityType<?>> TILEENTITY_REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, GIRCRedstoneMain.MODID);
-	
-	public static RegistryObject<Block> RS_ACCEPTOR = internalRegisterBlock("acceptor", () -> new BlockRedstoneAcceptor(BlockBehaviour.Properties.of(Material.METAL).strength(1.5f, 6.0f).requiresCorrectToolForDrops()));
-	public static RegistryObject<Block> RS_EMITTER = internalRegisterBlock("emitter", () -> new BlockRedstoneEmitter(BlockBehaviour.Properties.of(Material.METAL).strength(1.5f, 6.0f).requiresCorrectToolForDrops()));
-	
-	public static RegistryObject<Item> RS_LINKER = ITEM_REGISTRY.register("linker", () -> new Linkingtool(new Properties().tab(CreativeModeTab.TAB_REDSTONE)));
-	
-	public static RegistryObject<BlockEntityType<?>> EMITER_TILE = TILEENTITY_REGISTRY.register("emitter", () -> BlockEntityType.Builder.of(TileRedstoneEmitter::new, RS_EMITTER.get()).build(null));
-	
-	private static RegistryObject<Block> internalRegisterBlock(final String name, final Supplier<Block> sup) {
-		final RegistryObject<Block> registerObject = BLOCK_REGISTRY.register(name, sup);
-		ITEM_REGISTRY.register(name, () -> new BlockItem(registerObject.get(), new Properties().tab(CreativeModeTab.TAB_REDSTONE)));
-		return registerObject;
-	}
-	
-	public static void init() {
-		final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		ITEM_REGISTRY.register(bus);
-		BLOCK_REGISTRY.register(bus);
-		TILEENTITY_REGISTRY.register(bus);
-	}
-	
+
+    public static final DeferredRegister<Item> ITEM_REGISTRY = DeferredRegister
+            .create(ForgeRegistries.ITEMS, GIRCRedstoneMain.MODID);
+    public static final DeferredRegister<Block> BLOCK_REGISTRY = DeferredRegister
+            .create(ForgeRegistries.BLOCKS, GIRCRedstoneMain.MODID);
+    public static final DeferredRegister<BlockEntityType<?>> TILEENTITY_REGISTRY = DeferredRegister
+            .create(ForgeRegistries.BLOCK_ENTITIES, GIRCRedstoneMain.MODID);
+
+    public static RegistryObject<Block> RS_ACCEPTOR = internalRegisterBlock("acceptor",
+            () -> new BlockRedstoneAcceptor(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(1.5f, 6.0f).requiresCorrectToolForDrops()));
+    public static RegistryObject<Block> RS_EMITTER = internalRegisterBlock("emitter",
+            () -> new BlockRedstoneEmitter(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(1.5f, 6.0f).requiresCorrectToolForDrops()));
+
+    public static RegistryObject<Item> RS_LINKER = ITEM_REGISTRY.register("linker",
+            () -> new Linkingtool(new Properties().tab(CreativeModeTab.TAB_REDSTONE)));
+    public static final RegistryObject<Item> REMOTE_ACTIVATOR = ITEM_REGISTRY.register("activator",
+            () -> new RemoteActivator(new Properties().tab(CreativeModeTab.TAB_REDSTONE)));
+
+    public static RegistryObject<BlockEntityType<?>> EMITER_TILE = TILEENTITY_REGISTRY
+            .register("emitter", () -> BlockEntityType.Builder
+                    .of(TileRedstoneEmitter::new, RS_EMITTER.get()).build(null));
+
+    private static RegistryObject<Block> internalRegisterBlock(final String name,
+            final Supplier<Block> sup) {
+        final RegistryObject<Block> registerObject = BLOCK_REGISTRY.register(name, sup);
+        ITEM_REGISTRY.register(name, () -> new BlockItem(registerObject.get(),
+                new Properties().tab(CreativeModeTab.TAB_REDSTONE)));
+        return registerObject;
+    }
+
+    public static void init() {
+        final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ITEM_REGISTRY.register(bus);
+        BLOCK_REGISTRY.register(bus);
+        TILEENTITY_REGISTRY.register(bus);
+    }
+
 }
