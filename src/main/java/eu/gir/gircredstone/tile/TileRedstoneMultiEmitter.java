22cf543e085bbdf8c86fd5fc9ae4a35954953d0d
TimeJudge1337 <weber.kev1337@gmail.com>
2022-10-06 22:14:39 +0200

    feat: implemented Link

--- src/main/java/eu/gir/gircredstone/tile/TileRedstoneMultiEmitter.java
+++ src/main/java/eu/gir/gircredstone/tile/TileRedstoneMultiEmitter.java
@@ -1,12 +1,18 @@
 package eu.gir.gircredstone.tile;
 
+import com.mojang.authlib.minecraft.TelemetrySession;
 import eu.gir.gircredstone.init.GIRCInit;
 import eu.gir.linkableapi.ILinkableTile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TileRedstoneMultiEmitter extends BlockEntity implements ILinkableTile {
+
+    private List<BlockPos> listOfPositions = new ArrayList<>();
 
     public TileRedstoneMultiEmitter(final BlockPos pos, final BlockState state) {
         super(GIRCInit.MULTI_EMITER_TILE.get(), pos, state);
@@ -19,6 +25,10 @@ public class TileRedstoneMultiEmitter extends BlockEntity implements ILinkableTi
 
     @Override
     public boolean link(final BlockPos pos) {
+        if (pos != null && !listOfPositions.contains(pos)) {
+            listOfPositions.add(pos);
+            return true;
+        }
         return false;
     }
 
