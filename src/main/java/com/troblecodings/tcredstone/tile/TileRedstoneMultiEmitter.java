93eec873f259ce54da03629284a3a30f9cfcedad
Philipp <philippdal@gmail.com>
2022-12-14 17:57:44 +0100

    fix: read and write to nbt

--- src/main/java/com/troblecodings/tcredstone/tile/TileRedstoneMultiEmitter.java
+++ src/main/java/com/troblecodings/tcredstone/tile/TileRedstoneMultiEmitter.java
@@ -9,36 +9,46 @@ import com.troblecodings.tcredstone.init.GIRCInit;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class TileRedstoneMultiEmitter extends BlockEntity implements ILinkableTile {
 
-    private final List<BlockPos> listOfPositions = new ArrayList<>();
-    
-    private static final String ID_X = "xLinkedPos";
-    private static final String ID_Y = "yLinkedPos";
-    private static final String ID_Z = "zLinkedPos";
+    private List<BlockPos> listOfPositions = new ArrayList<>();
+
+    private static final String LINKED_POS_LIST = "linkedPos";
 
     public TileRedstoneMultiEmitter(final BlockPos pos, final BlockState state) {
         super(GIRCInit.MULTI_EMITER_TILE.get(), pos, state);
     }
-    
-    public static CompoundTag writeBlockPosToNBT(final BlockPos pos, final CompoundTag compound) {
+
+    public CompoundTag writeBlockPosToNBT(final List<BlockPos> pos, final CompoundTag compound) {
         if (pos != null && compound != null) {
-            compound.putInt(ID_X, pos.getX());
-            compound.putInt(ID_Y, pos.getY());
-            compound.putInt(ID_Z, pos.getZ());
+            
+            final ListTag list = new ListTag();
+            listOfPositions.forEach(BlockPos -> {
+                final CompoundTag item = NbtUtils.writeBlockPos(BlockPos);
+                list.add(item);
+            });
+            compound.put(LINKED_POS_LIST, list);
+            System.out.println("List: " + listOfPositions);
+            System.out.println("Comp: " + compound);
         }
         return compound;
     }
 
-    public static BlockPos readBlockPosFromNBT(final CompoundTag compound) {
-        if (compound != null && compound.contains(ID_X) && compound.contains(ID_Y)
-                && compound.contains(ID_Z)) {
-            return new BlockPos(compound.getInt(ID_X), compound.getInt(ID_Y),
-                    compound.getInt(ID_Z));
+    public List<BlockPos> readBlockPosFromNBT(final CompoundTag compound) {
+        final ListTag list = (ListTag) compound.get(LINKED_POS_LIST);
+        if (list != null) {
+            listOfPositions.clear();
+            list.forEach(pos -> {
+                final CompoundTag item = (CompoundTag) pos;
+                listOfPositions.add(NbtUtils.readBlockPos(item));
+            });
+            return listOfPositions;
         }
         return null;
     }
@@ -72,15 +82,15 @@ public class TileRedstoneMultiEmitter extends BlockEntity implements ILinkableTi
     @Override
     public void load(final CompoundTag compound) {
         super.load(compound);
-        listOfPositions.forEach(BlockPos -> readBlockPosFromNBT(compound));
+        this.listOfPositions = readBlockPosFromNBT(compound);
     }
 
     @Override
     protected void saveAdditional(final CompoundTag compound) {
         super.saveAdditional(compound);
-        listOfPositions.forEach(BlockPos -> writeBlockPosToNBT(BlockPos, compound));
+        writeBlockPosToNBT(listOfPositions, compound);
     }
-    
+
     public void redstoneUpdate(final boolean enabled) {
         listOfPositions.forEach(BlockPos -> redstoneUpdate(enabled, BlockPos, level));
     }
@@ -95,7 +105,7 @@ public class TileRedstoneMultiEmitter extends BlockEntity implements ILinkableTi
         }
         return enabled;
     }
-    
+
     public static boolean redstoneUpdate(final BlockPos linkedpos, final Level level) {
         if (linkedpos != null) {
             final BlockState state = level.getBlockState(linkedpos);
