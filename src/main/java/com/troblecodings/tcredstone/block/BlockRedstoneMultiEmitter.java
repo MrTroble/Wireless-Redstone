a24ca16342c181f5315ac355a4816fc2d1b7e45c
Philipp <philippdal@gmail.com>
2022-12-14 12:45:09 +0100

    fix: merge conficts

--- src/main/java/com/troblecodings/tcredstone/block/BlockRedstoneMultiEmitter.java
+++ src/main/java/com/troblecodings/tcredstone/block/BlockRedstoneMultiEmitter.java
@@ -3,8 +3,16 @@ package com.troblecodings.tcredstone.block;
 import com.troblecodings.tcredstone.tile.TileRedstoneMultiEmitter;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.contents.TranslatableContents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class BlockRedstoneMultiEmitter extends BlockRedstoneEmitter {
 
@@ -16,4 +24,48 @@ public class BlockRedstoneMultiEmitter extends BlockRedstoneEmitter {
     public BlockEntity newBlockEntity(final BlockPos pos, final BlockState state) {
         return new TileRedstoneMultiEmitter(pos, state);
     }
+
+    @Override
+    public InteractionResult use(final BlockState state, final Level world, final BlockPos pos,
+            final Player player, final InteractionHand hand, final BlockHitResult hit) {
+        if (world.isClientSide)
+            return InteractionResult.PASS;
+        if (player.getItemInHand(hand).getItem().equals(GIRCInit.RS_LINKER.get()))
+            return InteractionResult.PASS;
+        final BlockEntity entity = world.getBlockEntity(pos);
+        if (entity instanceof TileRedstoneMultiEmitter) {
+            final TileRedstoneMultiEmitter emitter = (TileRedstoneMultiEmitter) entity;
+            final List<BlockPos> listOfPositions = emitter.getLinkedPos();
+            if (listOfPositions == null) {
+                player.sendSystemMessage(
+                        MutableComponent.create(new TranslatableContents("em.notlinked")));
+            } else {
+                if (player.isCrouching()) {
+                    emitter.unlink();
+                    listOfPositions.forEach(BlockPos -> player.sendSystemMessage(
+                            MutableComponent.create(new TranslatableContents("em.unlink",
+                                    BlockPos.getX(), BlockPos.getY(), BlockPos.getZ()))));
+                } else {
+                    listOfPositions.forEach(BlockPos -> player.sendSystemMessage(
+                            MutableComponent.create(new TranslatableContents("lt.linkedpos",
+                                    BlockPos.getX(), BlockPos.getY(), BlockPos.getZ()))));
+                }
+            }
+            return InteractionResult.SUCCESS;
+        }
+        return InteractionResult.FAIL;
+    }
+
+    @Override
+    public void neighborChanged(final BlockState state, final Level world, final BlockPos pos,
+            final Block blockIn, final BlockPos fromPos, final boolean isMoving) {
+        if (world.isClientSide)
+            return;
+        final BlockEntity entity = world.getBlockEntity(pos);
+        if (entity instanceof TileRedstoneMultiEmitter) {
+            final TileRedstoneMultiEmitter emitter = (TileRedstoneMultiEmitter) entity;
+            emitter.redstoneUpdate(world.hasNeighborSignal(pos));
+        }
+    }
+
 }
--- src/main/java/com/troblecodings/tcredstone/tile/TileRedstoneMultiEmitter.java
+++ src/main/java/com/troblecodings/tcredstone/tile/TileRedstoneMultiEmitter.java
@@ -7,6 +7,9 @@ import com.troblecodings.linkableapi.ILinkableTile;
 import com.troblecodings.tcredstone.init.GIRCInit;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -40,4 +43,46 @@ public class TileRedstoneMultiEmitter extends BlockEntity implements ILinkableTi
         return true;
     }
 
+    public List<BlockPos> getLinkedPos() {
+        return this.listOfPositions;
+    }
+
+    @Override
+    public void load(final CompoundTag compound) {
+        super.load(compound);
+        listOfPositions.forEach(BlockPos -> NbtUtils.readBlockPos(compound));
+    }
+
+    @Override
+    protected void saveAdditional(final CompoundTag compound) {
+        super.saveAdditional(compound);
+        listOfPositions.forEach(BlockPos -> NbtUtils.writeBlockPos(BlockPos));
+    }
+    
+    public void redstoneUpdate(final boolean enabled) {
+        listOfPositions.forEach(BlockPos -> redstoneUpdate(enabled, BlockPos, level));
+    }
+
+    public static boolean redstoneUpdate(final boolean enabled, final BlockPos linkedpos,
+            final Level level) {
+        if (linkedpos != null) {
+            final BlockState state = level.getBlockState(linkedpos);
+            if (state.getBlock() instanceof BlockRedstoneAcceptor) {
+                level.setBlock(linkedpos, state.setValue(BlockRedstoneAcceptor.POWER, enabled), 3);
+            }
+        }
+        return enabled;
+    }
+    
+    public static boolean redstoneUpdate(final BlockPos linkedpos, final Level level) {
+        if (linkedpos != null) {
+            final BlockState state = level.getBlockState(linkedpos);
+            if (state.getBlock() instanceof BlockRedstoneAcceptor) {
+                final boolean newState = !state.getValue(BlockRedstoneAcceptor.POWER);
+                level.setBlock(linkedpos, state.setValue(BlockRedstoneAcceptor.POWER, newState), 3);
+                return newState;
+            }
+        }
+        return false;
+    }
 }
