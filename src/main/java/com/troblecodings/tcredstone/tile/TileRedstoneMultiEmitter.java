c0c860c5ef3520ced6629953e2bca0c91720807b
Philipp <philippdal@gmail.com>
2022-12-14 14:26:27 +0100

    feat: added multi-emitter

--- src/main/java/com/troblecodings/tcredstone/tile/TileRedstoneMultiEmitter.java
+++ src/main/java/com/troblecodings/tcredstone/tile/TileRedstoneMultiEmitter.java
@@ -9,7 +9,6 @@ import com.troblecodings.tcredstone.init.GIRCInit;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtUtils;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,9 +16,31 @@ import net.minecraft.world.level.block.state.BlockState;
 public class TileRedstoneMultiEmitter extends BlockEntity implements ILinkableTile {
 
     private final List<BlockPos> listOfPositions = new ArrayList<>();
+    
+    private static final String ID_X = "xLinkedPos";
+    private static final String ID_Y = "yLinkedPos";
+    private static final String ID_Z = "zLinkedPos";
 
     public TileRedstoneMultiEmitter(final BlockPos pos, final BlockState state) {
         super(GIRCInit.MULTI_EMITER_TILE.get(), pos, state);
+    }
+    
+    public static CompoundTag writeBlockPosToNBT(final BlockPos pos, final CompoundTag compound) {
+        if (pos != null && compound != null) {
+            compound.putInt(ID_X, pos.getX());
+            compound.putInt(ID_Y, pos.getY());
+            compound.putInt(ID_Z, pos.getZ());
+        }
+        return compound;
+    }
+
+    public static BlockPos readBlockPosFromNBT(final CompoundTag compound) {
+        if (compound != null && compound.contains(ID_X) && compound.contains(ID_Y)
+                && compound.contains(ID_Z)) {
+            return new BlockPos(compound.getInt(ID_X), compound.getInt(ID_Y),
+                    compound.getInt(ID_Z));
+        }
+        return null;
     }
 
     @Override
@@ -51,13 +72,13 @@ public class TileRedstoneMultiEmitter extends BlockEntity implements ILinkableTi
     @Override
     public void load(final CompoundTag compound) {
         super.load(compound);
-        listOfPositions.forEach(BlockPos -> NbtUtils.readBlockPos(compound));
+        listOfPositions.forEach(BlockPos -> readBlockPosFromNBT(compound));
     }
 
     @Override
     protected void saveAdditional(final CompoundTag compound) {
         super.saveAdditional(compound);
-        listOfPositions.forEach(BlockPos -> NbtUtils.writeBlockPos(BlockPos));
+        listOfPositions.forEach(BlockPos -> writeBlockPosToNBT(BlockPos, compound));
     }
     
     public void redstoneUpdate(final boolean enabled) {
