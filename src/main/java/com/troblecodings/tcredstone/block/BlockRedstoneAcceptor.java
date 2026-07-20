d9bb1ec7717df7bfa2e5d33d0a38304ff4a4bcd1
Philipp <philippdal@gmail.com>
2026-07-20 19:06:46 +0200

    fix: multilinking tooltag, correct redstone update

--- src/main/java/com/troblecodings/tcredstone/block/BlockRedstoneAcceptor.java
+++ src/main/java/com/troblecodings/tcredstone/block/BlockRedstoneAcceptor.java
@@ -10,33 +10,27 @@ import net.minecraft.world.BlockView;
 
 public class BlockRedstoneAcceptor extends Block {
 
-	public static final BooleanProperty POWER = BooleanProperty.of("power");
+    public static final BooleanProperty POWER = BooleanProperty.of("power");
 
-	public BlockRedstoneAcceptor(final Settings properties) {
-		super(properties);
-		this.setDefaultState(this.getDefaultState().with(POWER, false));
-	}
+    public BlockRedstoneAcceptor(final Settings properties) {
+        super(properties);
+        this.setDefaultState(this.getDefaultState().with(POWER, false));
+    }
 
-	@Override
-	public boolean emitsRedstonePower(final BlockState blockState) {
-		return true;
-	}
+    @Override
+    public boolean emitsRedstonePower(final BlockState blockState) {
+        return true;
+    }
 
-	@Override
-	public int getStrongRedstonePower(final BlockState blockState, final BlockView world, final BlockPos pos,
-			final Direction direction) {
-		return this.getWeakRedstonePower(blockState, world, pos, direction);
-	}
+    @Override
+    public int getWeakRedstonePower(final BlockState blockState, final BlockView world,
+            final BlockPos pos, final Direction direction) {
+        return blockState.get(POWER) ? 15 : 0;
+    }
 
-	@Override
-	public int getWeakRedstonePower(final BlockState blockState, final BlockView world, final BlockPos pos,
-			final Direction direction) {
-		return blockState.get(POWER) ? 15 : 0;
-	}
-
-	@Override
-	protected void appendProperties(final Builder<Block, BlockState> builder) {
-		builder.add(POWER);
-	}
+    @Override
+    protected void appendProperties(final Builder<Block, BlockState> builder) {
+        builder.add(POWER);
+    }
 
 }
