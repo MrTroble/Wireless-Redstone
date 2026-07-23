42f65361222cf6b787640a22d87826ca888deab7
Philipp <philippdal@gmail.com>
2026-07-23 18:03:02 +0200

    ref: update linkable api; fix merge conflicts

--- src/main/java/com/troblecodings/tcredstone/init/TCInit.java
+++ src/main/java/com/troblecodings/tcredstone/init/TCInit.java
@@ -56,22 +56,27 @@ public class TCInit {
         return level.getBlockState(pos).getBlock() instanceof BlockRedstoneAcceptor;
     }
 
-    public static final DeferredItem<Linkingtool> RS_LINKER = ITEM_REGISTRY.registerItem("linker",
-            props -> new Linkingtool(props, null, TCInit::acceptAcceptor), new Item.Properties());
-    public static final DeferredItem<MultiLinkingTool> RS_MULTILINKER = ITEM_REGISTRY.registerItem(
-            "multilinker", props -> new MultiLinkingTool(props, null, TCInit::acceptAcceptor),
-            new Item.Properties());
+    public static final DeferredItem<Linkingtool> RS_LINKER =
+            ITEM_REGISTRY.registerItem("linker", props -> new Linkingtool(props, null,
+                    TCInit::acceptAcceptor, TCRedstoneMain.COMPOUND_DATA), new Item.Properties());
+    public static final DeferredItem<MultiLinkingTool> RS_MULTILINKER =
+            ITEM_REGISTRY.registerItem(
+                    "multilinker", props -> new MultiLinkingTool(props, null,
+                            TCInit::acceptAcceptor, TCRedstoneMain.COMPOUND_DATA),
+                    new Item.Properties());
     public static final DeferredItem<RemoteActivator> REMOTE_ACTIVATOR = ITEM_REGISTRY.registerItem(
             "activator", props -> new RemoteActivator(props, null, TCInit::acceptAcceptor),
             new Item.Properties());
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> EMITER_TILE =
-            TILEENTITY_REGISTRY.register("emitter", () -> new BlockEntityType<>(
-                    TileRedstoneEmitter::new, Set.of(RS_EMITTER.get())));
+            TILEENTITY_REGISTRY.register("emitter",
+                    () -> new BlockEntityType<>(TileRedstoneEmitter::new,
+                            Set.of(RS_EMITTER.get())));
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> MULTI_EMITER_TILE =
-            TILEENTITY_REGISTRY.register("multiemitter", () -> new BlockEntityType<>(
-                    TileRedstoneMultiEmitter::new, Set.of(RS_MULTI_EMITTER.get())));
+            TILEENTITY_REGISTRY.register("multiemitter",
+                    () -> new BlockEntityType<>(TileRedstoneMultiEmitter::new,
+                            Set.of(RS_MULTI_EMITTER.get())));
 
     private static <T extends Block> DeferredBlock<T> internalRegisterBlock(final String name,
             final Function<BlockBehaviour.Properties, T> factory,
